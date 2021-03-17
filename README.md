ae3df467a43ed4cbea2ebed34bcfb94d7ba294cc
Bahador <bahador@rtnest.ca>
2021-03-17 10:50:45 -0600

    initiate the preprocessing and other priliminaries of the project

---
+++ README.md
@@ -0,0 +1,26 @@
+# Intelligent Fault diagnosis using Reinforcement Learning
+
+** Problem Statement: **
+Perform condition classification using an intelligent agent that learns the classification similar to perception of a human
+
+** Methodology:**
+Extract latent features using stacked autoencoders and use deep Q-Network to train the agent
+
+## Dataset
+The fault data of rolling bearings used in our study are collected by Case Western Reserve University (CWRU). The dataset consists of
+ball bearing test data for both normal and faulty bearings. The sampling frequency for the measured data is 48KHz that is comming from a measurement near the motor shaft in the experimental setup.
+
+* There are four different conditions:
+  * N: Normal
+  * IF: Inner fault
+  * OF: Outer fault
+  * RF: Roller fault
+
+* To validate the proposed method, the vibration data are divided into four sets (A, B, C, and D) according to the amount of loads induces to the shaft as an operating condition which will result in different vibration mode accroding to the dynamic of the shaft.
+
+* Each set (e.g. A) contains 10 different classes, (1,2,...,10) according to the fault diameter and fault location.
+* A, B, C contains all the classes and there are no classes that are unseen in these three datasets.
+* In average, each class under a certain dataset contains 480,000 samples. In other words, under that class condition the lab equipment was run and average of 480,000 observation from vibration sensor was captured.
+* Since the measurements are in time-domain, we need to convert them to frequecy domain to be able to extract frequency charactristics for any classification analysis. With this aim, we apply Fast fourieri transformation(FFT) on each 480,000 time-based samples. To achieve this we need to stride a window with a lenth W and steping size L. According to the literature, we consider L = W = 2400 samples. Accordingly, the result of FFT on each window with length of 2400 will be 2400. Hiowever due to the symetricity conditoin of FFT, we only consider the second half of the result which will be 1200 samples in frequency domain. After finishing this basic pre-processing analysis we will end up having an average of ~200 rows with 1200 columns (i.e. coulmns are the result of fft and will be treated as features). This matrix will be for each class under each dataset and can be used for the Benchmark analysis.
+**Note:** To achieve this, a function is defined in the pre-processsing class
+
---
+++ pre_processing.py
@@ -0,0 +1,82 @@
+#%%
+import scipy.io as spio
+import numpy as np
+import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import normalize
+
+
+
+def Data_Preprocess(Data, slice_size=1000, L = 120, window=False, random=42 , divide=0.33):
+    '''
+        Param=> Data: Data could be DataSetA.mat , DataSetB.mat, DataSetC.mat, DataSetD.mat
+        Param=> slice_size: The length of the window
+        Param=> L: The stride steping size
+        Param=> window: is False: we will have slicing mode with cronological slide_size -
+                        is True: We will have strides with window size of "slice_size" and stride length of "L"
+    '''
+    mat = spio.loadmat(Data, squeeze_me=True) # Load the dataset
+    data = {}
+    Raw= []
+    ##-----------import each data set into the data  
+    for i in range(1,len(mat)-2): 
+      data[i-1]=mat.get('C{}'.format(i))
+      # print("Class : ",(i-1)%10, "Imported :)")
+    
+    ##-----------When there is no stride
+    X_ALL = []
+    Y_ALL = []
+    if window == False : 
+        for element in data :
+            size = int(len(data[element])//slice_size) ## the number of windows in each data
+            X=[]
+            Y=[]
+            for k in range(size):
+                X.append(np.array(data[element][(k)*slice_size:(k+1)*slice_size]))
+                Y.append(element%10)
+            X_ALL.append(X)
+            Y_ALL.append(Y)
+            X=[]
+            Y=[]
+    ##-----------When the strides is active
+    else :
+        for element in data :
+            size = int((len(data[element]) - slice_size)/L) + 1 ## number of batches  
+            X=[]
+            Y=[]
+            for k in range(size):
+                X.append(np.array(data[element][int(k*(L)):int(k*L+slice_size)])) ## for each window it strides with windowsize/2
+                Y.append(element%10)
+            X_ALL.append(X)
+            Y_ALL.append(Y)
+            print("Class {} has shape : ".format(element%10),np.shape(X))
+            X=[]
+            Y=[]
+    
+    Flat_X =[]
+    Flat_Y =[]
+    FFT_X =[]
+    for element in X_ALL:
+        for things in element :
+            Flat_X.append(things)
+            fft_signal = np.fft.fft(things)
+            FFT_X.append(abs(fft_signal.real[0:len(fft_signal.real)//2]))
+    for element in Y_ALL:
+        for things in element :
+            Flat_Y.append(things)
+
+    X_train, X_test, y_train, y_test = train_test_split(np.asarray(normalize(FFT_X)),np.asarray(Flat_Y), test_size=divide, random_state=random,shuffle=True)
+    print("X_TRAIN has shape:",np.shape(X_train ))
+    print("Y_TRAIN has shape:",np.shape(y_train ))
+    print("X_TEST has shape:",np.shape(X_test ))
+    print("Y_TEST has shape:",np.shape(y_test ))
+    print("Classes are :",np.unique(y_train))
+    return X_train, X_test, y_train, y_test
+
+X_train, X_test, y_train, y_test = Data_Preprocess('DataSetA.mat',slice_size=1200, L=120, window=False)
+
+
+
+
+
+
